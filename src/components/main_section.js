6c9678b79eb80aa075e506ab7ec1021d288aa8b4
Rupesh0913 <k.rupeshsinha0913@gmail.com>
2021-07-02 10:17:47 +0530

    2nd update

--- src/components/main_section.js
+++ src/components/main_section.js
@@ -1,19 +1,24 @@
 import "./main_section.css"
-import React from "react"
+import React ,{useEffect} from "react"
 import builds from "../components/images/1.jpg"
-function MainSection({imgStart, Heading, Discription, Image}){
+import Aos from "aos";
+import "aos/dist/aos.css";
+function MainSection({imgStart, Heading, Discription, Image,}){
+    useEffect(()=>{
+        Aos.init({duration:1000});
+    },[]);
  return(
      <div>
-            <dvi className="main-section-container">
-                <div className="main-section-container-wrapper">
+         <div className="main-section-container">
+                <div  className="main-section-container-wrapper">
                     <div className="row" style={{display:'flex', flexDirection: imgStart === "start" ? "row-reverse":"row" }}>
                     <div className="col">
-                            <div className="image-wrapper">
+                            <div data-aos="fade-right" className="image-wrapper">
                                 <img src={builds} alt="image" className="img-info"/>
                             </div>
                         </div>
                         <div className="col">
-                            <div className="text-wrapper">
+                            <div  data-aos="zoom-in-up" data-aos-delay="1000" className="text-wrapper">
                                 <h1 className="heading">{Heading}</h1>
                                 <p className="discription">
                                     {Discription}
@@ -22,7 +27,7 @@ function MainSection({imgStart, Heading, Discription, Image}){
                         </div>
                     </div>
                 </div>
-            </dvi>
+            </div>
      </div>
  )
 }
--- src/components/pages/data.js
+++ src/components/pages/data.js
@@ -1,21 +1,22 @@
+import { useParams } from "react-router-dom";
 
 export const Data1 = {
 
     Heading:"1BHK",
-    Discription:"lorem fojwoiwjf wofjoij woepifjwf",
-    imgStart:""
+    Discription:"lorem fojwoiwjf wofjoij woepifjwf fghreherhdergedvbdv rgaergesr  er ergeg tresrg ",
+    imgStart:"",
 };
 
 export const Data2 = {
 
-    Heading:"1BHK",
-    Discription:"lorem fojwoiwjf wofjoij woepifjwf",
+    Heading:"2BHK",
+    Discription:"lorem fojwoiwjf wofjoij woepifjwf fghreherhdergedvbdv rgaergesr  er ergeg tresrg ",
     imgStart:""
 };
 
 export const Data3 = {
 
-    Heading:"1BHK",
-    Discription:"lorem fojwoiwjf wofjoij woepifjwf",
+    Heading:"3BHK",
+    Discription:"lorem fojwoiwjf wofjoij woepifjwf fghreherhdergedvbdv rgaergesr  er ergeg tresrg ",
     imgStart:""
 };
